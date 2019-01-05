1fa9fab4c4daa770e302387390f5f328664fc00d
pooja goyal <poojagoyal@poojas-MacBook-Air.local>
2019-01-04 22:31:53 -0800

    Initial Commit

---
+++ Hackerrank/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hackerrank
+//
+//  Created by pooja goyal on 1/4/19.
+//  Copyright © 2019 pooja goyal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
