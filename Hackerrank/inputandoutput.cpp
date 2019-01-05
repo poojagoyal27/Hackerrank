27925a8b3e82c3bf816529f87a67cde330859cfc
pooja goyal <poojagoyal@poojas-MacBook-Air.local>
2019-01-04 22:47:27 -0800

    Input Output

--- Hackerrank/inputandoutput.cpp
+++ Hackerrank/inputandoutput.cpp
@@ -6,10 +6,19 @@
 //  Copyright © 2019 pooja goyal. All rights reserved.
 //
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main() {
+    
+    int x,y,z;
+    cin>>x>>y>>z;
+    cout<<x+y+z;
     return 0;
 }
+
